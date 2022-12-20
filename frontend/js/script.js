23a6011f6561eff91c247283101d1fb09e54e1b3
Marc Shebaby <marcshababy02@gmail.com>
2022-12-20 14:30:11 +0200

    fixing countdown

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -152,11 +152,11 @@ pages.populate()
 
 // music player
 
-var player = document.getElementById("player");
+let player = document.getElementById("player");
 let progress = document.getElementById("progress");
 let playbtn = document.getElementById("playbtn");
 
-var playpause = function () {
+let playpause = function () {
   if (player.paused) {
     player.play();
   } else {
@@ -211,3 +211,36 @@ direct2.addEventListener("click",function() {
     localStorage.clear();
     location.assign('./WeGoJim.html')
   })
+  // make countdown
+  let target_date = new Date().getTime() + (5500*3600*48); // set the countdown date
+let days, hours, minutes, seconds; // variables for time units
+
+let countdown = document.getElementById("tiles"); // get tag element
+
+getCountdown();
+
+setInterval(function () { getCountdown(); }, 1000);
+
+function getCountdown(){
+
+	// find the amount of "seconds" between now and target
+	let current_date = new Date().getTime();
+	let seconds_left = (target_date - current_date) / 1000;
+
+	days = pad( parseInt(seconds_left / 86400) );
+	seconds_left = seconds_left % 86400;
+		 
+	hours = pad( parseInt(seconds_left / 3600) );
+	seconds_left = seconds_left % 3600;
+		  
+	minutes = pad( parseInt(seconds_left / 60) );
+	seconds = pad( parseInt( seconds_left % 60 ) );
+
+	// format countdown string + set tag value
+	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+}
+
+function pad(n) {
+	return (n < 10 ? '0' : '') + n;
+}
+
